11460a934716f021abb05151fd8152bc32a2c3c7
SandroKE <koomealessandro@gmail.com>
2023-09-13 01:14:52 +0300

    initial commit

--- shopping/single.php
+++ shopping/single.php
@@ -3,6 +3,22 @@
 
 <?php
 
+
+$isLoggedIn = isset($_SESSION['user_id']);
+
+    if(isset($_POST['submit'])){
+        $pro_id = $_POST['pro_id'];
+        $pro_name = $_POST['pro_name'];
+        $pro_image = $_POST['pro_image'];
+        $pro_price = $_POST['pro_price'];
+        $pro_amount = $_POST['pro_amount'];
+        $pro_file = $_POST['pro_file'];
+        $user_id = $_POST['user_id'];
+
+        $sql = "INSERT INTO cart (pro_id,pro_name,pro_image,pro_price,pro_amount,pro_file,user_id)
+         VALUES(:pro_id,:pro_name,:pro_image,:pro_price,:pro_amount,:pro_file,:user_id)";
+    }
+
 if (isset($_GET['prod_id'])) {
     $id = $_GET['prod_id'];
 
@@ -70,10 +86,15 @@ if (isset($_GET['prod_id'])) {
                             <div class="">
                                 <input name="user_id" type="text" class="form-control" value="<?php echo $_SESSION['user_id'] ?>">
                             </div>
-                       
-                            <div class="cart mt-4 align-items-center"> 
-                                <button name="submit" type="submit " class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button> 
+
+                            <div class="cart mt-4 align-items-center">
+                                <?php if ($isLoggedIn) : ?>
+                                    <button name="submit" type="submit" class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button>
+                                <?php else : ?>
+                                    <button type="button" class="btn btn-secondary text-uppercase mr-2 px-4" disabled><i class="fas fa-shopping-cart"></i> Log in to add to cart</button>
+                                <?php endif; ?>
                             </div>
+
                         </form>
                     </div>
                 </div>
@@ -83,11 +104,22 @@ if (isset($_GET['prod_id'])) {
 </div>
 <?php require "../includes/footer.php"; ?>
 
+ <script>
+    $(document).ready(function() {
+        /*   console.log("Hello"); */
+        $(document).on("submit", function(e) {
+          var formdata = $("#formdata").serialize()+"&submit;&submit";
+        })
+    });
+</script> 
+
 <script>
-$(document).ready(function(){
-/*   console.log("Hello"); */
-    $(document).on("submit", function(e){
-        alert('Clicked');
-    })
+    $(document).ready(function(){
+    $(document).on("submit", "#form-data", function(e){
+        <?php if (!$isLoggedIn): ?>
+            e.preventDefault(); // Prevent form submission
+            alert('Please log in to add items to the cart.');
+        <?php endif; ?>
+    });
 });
 </script>
