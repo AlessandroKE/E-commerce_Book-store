c1f7c509069a8758dcb3a81ae5fd7307496c6103
SandroKE <koomealessandro@gmail.com>
2023-12-03 17:53:35 -0700

    Working on delete page

--- shopping/delete-item.php
+++ shopping/delete-item.php
@@ -2,6 +2,23 @@
 <?php require "../config/config.php"; ?>
 
 
+<?php
+
+if (isset($_POST['delte'])){
+    $id = $_POST['id'];
+
+    $delete = $conn->prepare("DELET FROM cart  WHERE id = '$id'");
+    $delete->execute();
+     // Execute the query
+/*      if($insert->execute()) {
+       // echo "Quantity updated successfully";
+    } else {
+        echo "Error updating record: ";
+    }
+ */
+
+}
+?>
 
 
 
